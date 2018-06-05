d40dc0ebe921912ed0e5e3994c06e48639e4d6aa
German <German>
2018-06-05 17:23:22 -0300

    Nombre de resultados

--- src/tpe/Test.java
+++ src/tpe/Test.java
@@ -6,24 +6,32 @@ public class Test {
 	public static void main(String[] args) {
 
 		LiteraryGenreAnalyzer tool= new LiteraryGenreAnalyzer();
-		tool.generateGenreGraph("C:/datasets-etapa2/dataset0.csv");
+		tool.generateGenreGraph("C:/datasets/dataset3.csv");
 		//System.out.println(tool.getBusquedaPorAristas());
 		
+		System.out.println("//////////Cantidad de generos///////////");
 		System.out.println(tool.getCantidadGeneros());
 		//tool.printGenreGraph();
 		
+		System.out.println("//////////Resultado servicio A///////////");
 		System.out.println(tool.servicioA(6, "cine"));
+		System.out.println("//////////Busqueda de generos///////////");
 		System.out.println(tool.getBusquedasGenero());
 		
 		//Servicio 2 y los contadores que arroja
+		System.out.println("//////////Resultado servicio B///////////");
 		System.out.println(tool.servicioB("cine"));
+		System.out.println("//////////Cantidad de entradas recursivas///////////");
 		System.out.println(tool.getEntradasRecursivasS2());
 		
 		
 		
 		//Servicio 3 y los contadores que arroja
+		System.out.println("//////////Resultado servicio C///////////");
 		System.out.println(tool.servicioC("cine"));
+		System.out.println("//////////Cantidad de entradas recursivas///////////");
 		System.out.println(tool.getEntradasRecursivasS3());
+		System.out.println("//////////Cantidad de comparaciones en la busqueda de generos afines///////////");
 		System.out.println(tool.getComparacionesCiclo());
 
 	}
